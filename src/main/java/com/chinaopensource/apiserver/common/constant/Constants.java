b25af92c236c25893fe4a94dffe48465373914dd
lizhongl <lizhongl@8531.cn>
2017-12-14 23:21:27 +0800

    增加常量

--- src/main/java/com/chinaopensource/apiserver/common/constant/Constants.java
+++ src/main/java/com/chinaopensource/apiserver/common/constant/Constants.java
@@ -21,5 +21,35 @@ public final class Constants {
     public static final String USERINFO_TOKEN = "token";
     // 用户信息 
     public static final String USERINFO_INFO = "info";
-    
+
+    /**
+     * smtp的主机名称
+     */
+    public static final String MAIL_SMTP_HOST_KEY = "mail.smtp.host";
+    public static final String MAIL_SMTP_HOST_QQ_VALUE = "mx1.qq.com";
+    /**
+     * 协议
+     */
+    public static final String MAIL_SMTP_PROTOCOL_KEY = "mail.store.protocol";
+    public static final String MAIL_SMTP_PROTOCOL_VALUE = "smtp";
+    /**
+     * smtp的port
+     */
+    public static final String MAIL_SMTP_PORT_KEY = "mail.smtp.port";
+    public static final Integer MAIL_SMTP_PORT_VALUE = 25;
+    /**
+     * smtp认证 参数名
+     */
+    public static final String MAIL_SMTP_AUTH_KEY = "mail.smtp.auth";
+    public static final String MAIL_SMTP_AUTH_VALUE = "true";
+    /**
+     * 邮件编码格式
+     */
+    public static final String EMAIL_ENCODE_PATTERN = "gb2312";
+    /**
+     * 邮件内容表编码格式
+     */
+    public static final String EMAIL_CONTENT_PATTERN = "text/html;charset=gb2312";
+
+
 }
