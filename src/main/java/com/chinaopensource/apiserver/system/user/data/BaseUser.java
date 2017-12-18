b2982f19c931da58952d4b934c213fe511508a42
lizhongl <907678041@qq.com>
2017-12-18 22:47:07 +0800

    删除多余代码

--- src/main/java/com/chinaopensource/apiserver/system/user/data/BaseUser.java
+++ src/main/java/com/chinaopensource/apiserver/system/user/data/BaseUser.java
@@ -1,12 +1,11 @@
 package com.chinaopensource.apiserver.system.user.data;
 
-import javax.validation.constraints.NotNull;
-
-import com.chinaopensource.apiserver.common.base.BaseDomain;
 import org.hibernate.validator.constraints.Length;
 
-public class BaseUser extends BaseDomain {
-	
+import javax.validation.constraints.NotNull;
+
+public class BaseUser {
+
 	int id;
 	@NotNull
 	@Length(min=6,max=20)
@@ -18,55 +17,68 @@ public class BaseUser extends BaseDomain {
 	String photo;
 	String address;
 	String email;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getLoginName() {
 		return loginName;
 	}
+
 	public void setLoginName(String loginName) {
 		this.loginName = loginName;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public String getPhoto() {
 		return photo;
 	}
+
 	public void setPhoto(String photo) {
 		this.photo = photo;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	
-
 }
