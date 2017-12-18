67d6fb996d3d36d04dbbb8c9ca3ad58d05017c82
lizhongl <907678041@qq.com>
2017-12-18 22:31:36 +0800

    修改获取配置文件的类，删除重复的类， 增加生成图片的工具类，完善配置文件

--- src/main/java/com/chinaopensource/apiserver/common/configure/OpenSourceConfig.java
+++ src/main/java/com/chinaopensource/apiserver/common/configure/OpenSourceConfig.java
@@ -1,134 +1,105 @@
 package com.chinaopensource.apiserver.common.configure;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 读取执行配置文件的类
  * create by lzl ON 2017/12/17
  */
-@Component
-@ConfigurationProperties(prefix = "email")
+@Configuration
+@ConfigurationProperties(prefix = "opensource")
 public class OpenSourceConfig {
 
-    /**
-     * smtp主机key
-     */
-    private String smtpHostKey;
-    /**
-     * smtp主机value
-     */
-    private String smtpHostValue;
-    /**
-     * smtp协议key
-     */
-    private String smtpProtocolKey;
-    /**
-     * smtp协议value
-     */
-    private String getSmtpProtocolValue;
-    /**
-     * smtp端口key
-     */
-    private String smtpPortKey;
-    /**
-     * smtp端口value
-     */
-    private String smtpPortValue;
-    /**
-     * smtp认证key
-     */
-    private String smtpAuthKey;
-    /**
-     * smtp认证value
-     */
-    private String smtpAuthValue;
-    /**
-     * 邮件编码格式
-     */
-    private String  encodePattern;
-    /**
-     * 邮件内容编码格式
-     */
-    private String contentPattern;
+    private Map<String,String> jwt = new HashMap<>();
+    private Map<String,Integer> image = new HashMap<>();
+    private Map<String,String> email = new HashMap<>();
 
-    public String getSmtpHostKey() {
-        return smtpHostKey;
+    public Map<String, String> getJwt() {
+        return jwt;
     }
 
-    public void setSmtpHostKey(String smtpHostKey) {
-        this.smtpHostKey = smtpHostKey;
+    public void setJwt(Map<String, String> jwt) {
+        this.jwt = jwt;
     }
 
-    public String getSmtpHostValue() {
-        return smtpHostValue;
+    public Map<String, Integer> getImage() {
+        return image;
     }
 
-    public void setSmtpHostValue(String smtpHostValue) {
-        this.smtpHostValue = smtpHostValue;
+    public void setImage(Map<String, Integer> image) {
+        this.image = image;
     }
 
-    public String getSmtpProtocolKey() {
-        return smtpProtocolKey;
+    public Map<String, String> getEmail() {
+        return email;
     }
 
-    public void setSmtpProtocolKey(String smtpProtocolKey) {
-        this.smtpProtocolKey = smtpProtocolKey;
+    public void setEmail(Map<String, String> email) {
+        this.email = email;
     }
 
-    public String getGetSmtpProtocolValue() {
-        return getSmtpProtocolValue;
+    public String getJwtHeader(){
+        return jwt.get("header");
     }
 
-    public void setGetSmtpProtocolValue(String getSmtpProtocolValue) {
-        this.getSmtpProtocolValue = getSmtpProtocolValue;
+    public String getJwtSecret(){
+        return jwt.get("secret");
     }
 
-    public String getSmtpPortKey() {
-        return smtpPortKey;
+    public Integer getJwtExpiration(){
+        return Integer.valueOf(jwt.get("expiration"));
     }
 
-    public void setSmtpPortKey(String smtpPortKey) {
-        this.smtpPortKey = smtpPortKey;
+    public Integer getImageWidth(){
+        return image.get("width");
     }
 
-    public String getSmtpPortValue() {
-        return smtpPortValue;
+    public Integer getImageHeight(){
+        return image.get("height");
     }
 
-    public void setSmtpPortValue(String smtpPortValue) {
-        this.smtpPortValue = smtpPortValue;
+    public String getEmailSmtpHostKey(){
+        return email.get("smtp-host-key");
     }
 
-    public String getSmtpAuthKey() {
-        return smtpAuthKey;
+    public String getEmailSmtpHostValue(){
+        return email.get("smtp-host-value");
     }
 
-    public void setSmtpAuthKey(String smtpAuthKey) {
-        this.smtpAuthKey = smtpAuthKey;
+    public String getEmailSmtpProtocolKey(){
+        return email.get("smtp-protocol-key");
     }
 
-    public String getSmtpAuthValue() {
-        return smtpAuthValue;
+    public String getEmailSmtpProtocolValue(){
+        return email.get("smtp-protocol-value");
     }
 
-    public void setSmtpAuthValue(String smtpAuthValue) {
-        this.smtpAuthValue = smtpAuthValue;
+    public String getEmailSmtpPortKey(){
+        return email.get("smtp-port-key");
     }
 
-    public String getEncodePattern() {
-        return encodePattern;
+    public String getEmailSmtPortValue(){
+        return email.get("smtp-port-value");
     }
 
-    public void setEncodePattern(String encodePattern) {
-        this.encodePattern = encodePattern;
+    public String getEmailSmtpAuthKey(){
+        return email.get("smtp-auth-key");
     }
 
-    public String getContentPattern() {
-        return contentPattern;
+    public String getEmailSmtpAuthValue(){
+        return email.get("smtp-auth-value");
     }
 
-    public void setContentPattern(String contentPattern) {
-        this.contentPattern = contentPattern;
+    public String getEmailEncodePattern(){
+        return email.get("encode-pattern");
     }
+
+    public String getEmailContentPattern(){
+        return email.get("content-pattern");
+    }
+
 }
